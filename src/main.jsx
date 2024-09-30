eb4229640b80e4fc8490be5e37498a7963b61d69
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-09-30 22:57:39 +0800

    feat: radix ui setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@radix-ui/themes/styles.css";
 import "./index.css";
+import { Theme } from "@radix-ui/themes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Theme></Theme>
   </StrictMode>
 );
