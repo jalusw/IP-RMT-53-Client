83b744ef6f29ae9c2854d78b9d10b7c4aa73d19a
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-09-30 23:00:18 +0800

    feat: react router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { Theme } from "@radix-ui/themes";
+
+import router from "../routes/router";
+
 import "@radix-ui/themes/styles.css";
 import "./index.css";
-import { Theme } from "@radix-ui/themes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Theme></Theme>
+    <Theme>
+      <RouterProvider router={router} />
+    </Theme>
   </StrictMode>
 );
