972089ead32c30e33aba9e763489d541cc8f12f1
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-01 21:36:23 +0800

    feat: add redux persist

--- src/config/store.js
+++ src/config/store.js
@@ -1,10 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { api } from "../services/api";
 
-export const store = configureStore({
-  reducer: {
+import { authSlice } from "../slices/authSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     [api.reducerPath]: api.reducer,
-  },
+    auth: authSlice.reducer,
+  }),
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }).concat(api.middleware),
 });
+
+export const persistor = persistStore(store);
