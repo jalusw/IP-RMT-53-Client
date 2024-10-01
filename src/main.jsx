4689a8b453bbb1516a1b52f4f37300c1d47a808e
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-02 06:52:32 +0800

    feat: configure redix persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,22 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { Theme, ThemePanel } from "@radix-ui/themes";
 import { Provider } from "react-redux";
-import { store } from "./config/store";
+import { persistor, store } from "./config/store";
 
 import router from "../routes/router";
 
 import "@radix-ui/themes/styles.css";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <Theme accentColor="indigo" appearance="dark">
-        <RouterProvider router={router} />
-      </Theme>
+      <PersistGate loading={null} persistor={persistor}>
+        <Theme accentColor="cyan" appearance="light">
+          <RouterProvider router={router} />
+        </Theme>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
