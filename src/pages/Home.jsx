9748869330aafd9305a81dda5b4ac4303f64c275
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-02 20:20:03 +0800

    feat: update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,26 @@
-import {
-  Container,
-  Text,
-  Heading,
-  Section,
-  Button,
-  Card,
-} from "@radix-ui/themes";
+import { Container, Text, Heading, Section, Button } from "@radix-ui/themes";
 
 export default function Home() {
   return (
     <>
-      <header>
+      <header className="py-28">
         <Section>
-          <Container>
+          <Container className="px-4">
             <Heading className="block text-6xl font-bold">
               Write anything with ease.
             </Heading>
-            <Text className="mt-4 block">
+            <Text className="mt-4 block max-w-[66ch] leading-loose">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               Doloremque corporis maxime, nobis reiciendis explicabo
               perspiciatis excepturi harum animi.
             </Text>
+            <Button variant="outline" mt="5" size="3">
+              Try it now
+            </Button>
           </Container>
         </Section>
       </header>
+      <main id="main"></main>
     </>
   );
 }
