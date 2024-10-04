d6456fc3fe4e851bf227de3982224f0c751d5f38
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-04 10:26:28 +0800

    feat: add responsive file tree support

--- src/components/utils/WorkspaceNavbar.jsx
+++ src/components/utils/WorkspaceNavbar.jsx
@@ -1,24 +1,45 @@
-import { Box, Flex, Section, Container } from "@radix-ui/themes";
+import { Box, Flex, Section, Container, Button } from "@radix-ui/themes";
 import ThemeSwitcher from "../buttons/ThemeSwitcher";
 import ProfileSetting from "../utils/ProfileSetting";
 import Logout from "../buttons/Logout";
+import { FileIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import WorkspaceFileTree from "./WorkspaceFileTree";
+import { useState } from "react";
 
 export default function WorkspaceNavbar() {
+  const [isFileTreeOpen, setIsFileTreeOpen] = useState(false);
   return (
     <Section asChild size="2">
       <nav>
         <Container className="px-8">
           <Flex justify="between" align="center">
-            <ProfileSetting />
-            <Box></Box>
-            <Flex asChild className="space-x-2">
+            <Box>
+              <Box className="fixed left-0 top-0 z-[100] w-[50%]">
+                <WorkspaceFileTree
+                  className={isFileTreeOpen ? "block" : "hidden"}
+                />
+              </Box>
+            </Box>
+            <Flex asChild className="space-x-4">
               <ul>
+                <li className="md:hidden">
+                  <Button
+                    variant="outline"
+                    className="cursor-pointer"
+                    onClick={() => {
+                      setIsFileTreeOpen(!isFileTreeOpen);
+                    }}
+                  >
+                    <FileIcon />
+                  </Button>
+                </li>
                 <li>
-                  <ThemeSwitcher />
+                  <ThemeSwitcher variant="outline" />
                 </li>
                 <li>
                   <Logout />
                 </li>
+                <ProfileSetting />
               </ul>
             </Flex>
           </Flex>
